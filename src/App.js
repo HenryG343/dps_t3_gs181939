5b50215d94cc14a8edcee9b61ae3c7db57714767
HenryG343 <85520862+HenryG343@users.noreply.github.com>
2022-08-22 21:13:30 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
+function ImagenAvatar(props){
+  return(
+    <>
+      <img src='https://images.8tracks.com/cover/i/010/264/850/Persona-5-Protagonist-Gameplay-Teaser-Trailer-9452.jpg?rect=627,6,652,652&q=98&fm=jpg&fit=max' height={200} width={200}/>
+    </>
+  )
+}
+
+function Datos(){
+  return(
+    <>
+      <br/>
+      <p><b>Nombre: </b>Henry Bryan Gutierrez Solorzano</p>
+      <p><b>Carrera: </b>Ingenieria en ciencias de la computacion</p>
+    </>
+  )
+}
+
+function Skills(){
+  return(
+    <>
+      <br/>
+      <p>Skill en programacion</p>
+      <ul>
+        <li>C#</li>
+        <li>React</li>
+        <li>Node js</li>
+      </ul>
+    </>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ImagenAvatar></ImagenAvatar>
+      <Datos></Datos>
+      <Skills></Skills>
+    </> 
   );
 }
 
